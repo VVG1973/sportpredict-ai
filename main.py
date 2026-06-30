9244c4df648f7e3daca89ab5bf7d6abb10ca84b4
Ваше Имя <-email@example.com>
2026-06-30 13:47:21 +0300

    Fix: surgically remove all unexpected indents in global scope

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from aiogram import Dispatcher
 from config import settings
 
 logging.basicConfig(
-    level=getattr(logging, settings.LOG_LEVEL),
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    stream=sys.stdout
+level=getattr(logging, settings.LOG_LEVEL),
+format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+stream=sys.stdout
 )
 logger = logging.getLogger(__name__)
 
